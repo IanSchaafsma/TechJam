be73627b317cda8ad9ce74f93e244a98a0ae0cf8
Ian <33319@ma-web.nl>
2023-09-07 12:20:56 +0200

    add comments explaining the code

--- loop.php
+++ loop.php
@@ -2,27 +2,26 @@
 
 
 
-for($number = 33; $number < 543; $number++){
+for($number = 33; $number < 543; $number++){ // make a variable number and loop it through 543
 
-    if($number % 5 == 0 && $number % 9 == 0){
+    if($number % 5 == 0 && $number % 9 == 0){ // if number is dividable by both 5 and 9 print Web 
         echo "Web </br>";
     }
 
-    elseif($number % 5 == 0){
+    elseif($number % 5 == 0){ // if number is dividable by 5 print Software
         echo "Software </br>";
     }
 
-    elseif($number % 2 == 0){
+    elseif($number % 2 == 0){ // if number is even print the number in bold
         echo "<b>$number</b> </br> \n";
     }
     
-    elseif($number % 9 == 0){
+    elseif($number % 9 == 0){ // if the number is dividable by 9 then print Developer
         echo "Developer </br> \n";
     }
 
-    else{
+    else{ // if none of the above are true then just print the regular number
         echo "$number </br> \n";
-        
     }
 
 }
